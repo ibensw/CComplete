ibensw/CComplete

commit f6a74e5d080d47736409458d08c315945baa9e4e

Add support for structs without a typedef

tokenizer.py
@@ -23,7 +23,7 @@ class Tokenizer:
     K_MEMBER = "m"
     # todo: complete list...
 
-    declre = re.compile('(const\s)*(\w+)\s*([\s\*])\s*(\w+)(\[.*\])?')
+    declre = re.compile('(const\s)*(struct\s)*(\w+)\s*([\s\*])\s*(\w+)(\[.*\])?')
 
     def __init__(self, cachepath = "/tmp", cachesize = 500):
         self.cachesize = cachesize
@@ -225,7 +225,7 @@ class Tokenizer:
         decl=decl.strip()
         r = Tokenizer.declre.match(decl)
         if r:
-            return (r.group(4), r.group(2), r.group(3) == "*", r.group(5))
+            return (r.group(5), r.group(3), r.group(3) == "*", r.group(6))
 
     @staticmethod
     def pretty_type(line):

commit 607b7d01b28ad13f18df6a5852741bf1d6cf10b4

Fix pointer detection broken in f6a74e5d080d47736409458d08c315945baa9e4e

tokenizer.py
@@ -249,7 +249,7 @@ class Tokenizer:
         decl=decl.strip()
         r = Tokenizer.declre.match(decl)
         if r:
-            return (r.group(5), r.group(3), r.group(3) == "*", r.group(6))
+            return (r.group(5), r.group(3), r.group(4) == "*", r.group(6))
 
     @staticmethod
     def pretty_type(line):